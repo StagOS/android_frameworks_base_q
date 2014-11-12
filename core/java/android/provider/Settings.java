de5f4a7dd1de3ab3bf96431cab3d0ee689b49266
htchoi1995 <htchoi1995@gmail.com>
2014-11-12 14:07:51 -0500

    Disable/Enable screenshot sound [1/2]
    Change-Id: I9f32d0026833f9350a2e8e4bedaff3801f508e76

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4584,6 +4584,15 @@ public final class Settings {
                 BOOLEAN_VALIDATOR;
 
         /**
+         * Enable/Disable screenshot sound
+         * @hide
+         */
+        public static final String SCREENSHOT_SOUND = "screenshot_sound";
+        /** @hide */
+        private static final Validator SCREENSHOT_SOUND_VALIDATOR =
+                BOOLEAN_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -4655,6 +4664,7 @@ public final class Settings {
             VOLUME_KEY_CURSOR_CONTROL,
             ENABLE_CONDITIONS,
             ENABLE_SUGGESTIONS,
+            SCREENSHOT_SOUND,
         };
 
         /**
@@ -4785,6 +4795,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VOLUME_KEY_CURSOR_CONTROL);
             PRIVATE_SETTINGS.add(ENABLE_CONDITIONS);
             PRIVATE_SETTINGS.add(ENABLE_SUGGESTIONS);
+            PRIVATE_SETTINGS.add(SCREENSHOT_SOUND);
         }
 
         /**
@@ -4889,6 +4900,7 @@ public final class Settings {
             VALIDATORS.put(VOLUME_KEY_CURSOR_CONTROL, VOLUME_KEY_CURSOR_CONTROL_VALIDATOR);
             VALIDATORS.put(ENABLE_CONDITIONS, ENABLE_CONDITIONS_VALIDATOR);
             VALIDATORS.put(ENABLE_SUGGESTIONS, ENABLE_SUGGESTIONS_VALIDATOR);
+            VALIDATORS.put(SCREENSHOT_SOUND, SCREENSHOT_SOUND_VALIDATOR);
         }
 
         /**
