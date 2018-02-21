495bf29a150286c830f256abb035e463c38cecc2
xyyx <xyyx@mail.ru>
2018-02-21 20:48:18 +0800

    HWKeys: Disable haptic on keyguard
    Change-Id: I0d584146c155de46c4ef3b5ada6c84e2976f2c52
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -4052,7 +4052,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 && (policyFlags & WindowManagerPolicy.FLAG_VIRTUAL) != 0
                 && (!isNavBarVirtKey || mNavBarVirtualKeyHapticFeedbackEnabled)
                 && event.getRepeatCount() == 0
-                && !isHwKeysDisabled();
+                && !isHwKeysDisabled()
+                && !keyguardOn();
 
         // Specific device key handling
         if (mDeviceKeyHandler != null) {
