2448018481ba9e9328628ae052b1f8cfecdff851
Lucchetto <lucchetto.tie@live.com>
2019-03-28 18:41:03 +0100

    base: Smart Charging (1/4)
    ZeNiXxX - Bring to Android 10
    
    Signed-off-by: ZeNiXxX <zenixxx.havoc@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Iceb1fb59ab26bc1fcbd9df4087c3679163c69712

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5216,6 +5216,16 @@ public final class Settings {
         public static final String DOZE_TRIGGER_DOUBLETAP = "doze_trigger_doubletap";
 
         /**
+         * @hide
+         */
+        public static final String SMART_CHARGING = "smart_charging";
+
+        /**
+         * @hide
+         */
+        public static final String SMART_CHARGING_LEVEL = "smart_charging_level";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
