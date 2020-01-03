6687d02a5be77b471c9c51a2775173c6197014b9
ezio84 <brabus84@gmail.com>
2020-01-03 21:26:04 +0100

    Power button flashlight toggle: rely on aosp doze sensors check code
    Till now we were checking proximity sensor before toggling the
    flashlight with our custom implementation.
    For doze pulsing, Google have a solid code implementation to check the
    same, and since some months they even made it better using a
    prox gated brightness sensor if available (e.g. Pixel phones).
    Let's just use it.
    
    Change-Id: I79a5985e4205417615f9630c7f3a2a17d9ee882b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -251,7 +251,6 @@ import com.android.systemui.statusbar.policy.ConfigurationController.Configurati
 import com.android.systemui.statusbar.policy.DeviceProvisionedController;
 import com.android.systemui.statusbar.policy.DeviceProvisionedController.DeviceProvisionedListener;
 import com.android.systemui.statusbar.policy.ExtensionController;
-import com.android.systemui.statusbar.policy.FlashlightController;
 import com.android.systemui.statusbar.policy.HeadsUpManager;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
 import com.android.systemui.statusbar.policy.KeyguardUserSwitcher;
@@ -650,7 +649,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         }
     };
 
-    private FlashlightController mFlashlightController;
     private KeyguardUserSwitcher mKeyguardUserSwitcher;
     protected UserSwitcherController mUserSwitcherController;
     private CurrentUserTracker mUserTracker;
@@ -1229,8 +1227,6 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         // Private API call to make the shadows look better for Recents
         ThreadedRenderer.overrideProperty("ambientRatio", String.valueOf(1.5f));
-
-        mFlashlightController = Dependency.get(FlashlightController.class);
     }
     private void adjustBrightness(int x) {
         mBrightnessChanged = true;
@@ -2353,12 +2349,7 @@ public class StatusBar extends SystemUI implements DemoMode,
 
     @Override
     public void toggleCameraFlash() {
-        if (mFlashlightController != null) {
-            mFlashlightController.initFlashLight();
-            if (mFlashlightController.hasFlashlight() && mFlashlightController.isAvailable()) {
-                mFlashlightController.setFlashlight(!mFlashlightController.isEnabled());
-            }
-        }
+        mDozeServiceHost.toggleCameraFlash();
     }
 
 
@@ -2990,10 +2981,6 @@ public class StatusBar extends SystemUI implements DemoMode,
         pw.println("SharedPreferences:");
         for (Map.Entry<String, ?> entry : Prefs.getAll(mContext).entrySet()) {
             pw.print("  "); pw.print(entry.getKey()); pw.print("="); pw.println(entry.getValue());
-        }
-
-        if (mFlashlightController != null) {
-            mFlashlightController.dump(fd, pw, args);
         }
     }
 
@@ -5083,6 +5070,12 @@ public class StatusBar extends SystemUI implements DemoMode,
 
         public boolean shouldAnimateScreenOff() {
             return mAnimateScreenOff;
+        }
+
+        public void toggleCameraFlash() {
+            for (Callback callback : mCallbacks) {
+                callback.toggleCameraFlash();
+            }
         }
     }
 
