3acee1b79574ce59a6e335c7e5ac9038f0526a18
vjspranav <pranavasri@live.in>
2020-01-03 22:08:47 +0000

    Compilation fixes on r20
    Change-Id: I6297018b95e002a3cf5e50322f6ae27efecfa8ea

--- packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleFactory.java
+++ packages/SystemUI/src/com/google/android/systemui/SystemUIGoogleFactory.java
@@ -18,12 +18,12 @@ import java.util.function.Consumer;
 
 public class SystemUIGoogleFactory extends SystemUIFactory {
     @Override
-    public ScrimController createScrimController(ScrimView scrimBehind, ScrimView scrimInFront,
+    public ScrimController createScrimController(ScrimView scrimBehind, ScrimView scrimInFront, ScrimView scrimBubble,
             LockscreenWallpaper lockscreenWallpaper,
             TriConsumer<ScrimState, Float, GradientColors> scrimStateListener,
             Consumer<Integer> scrimVisibleListener, DozeParameters dozeParameters,
             AlarmManager alarmManager, KeyguardMonitor keyguardMonitor) {
-        return new LiveWallpaperScrimController(scrimBehind, scrimInFront, lockscreenWallpaper,
+        return new LiveWallpaperScrimController(scrimBehind, scrimInFront, scrimBubble, lockscreenWallpaper,
                 scrimStateListener, scrimVisibleListener, dozeParameters, alarmManager, keyguardMonitor);
     }
 }
