42fcb8ce305730607129d0a2eb8df259db7654f7
spezi77 <spezi7713@gmx.net>
2020-01-05 14:26:55 +0100

    Move icon shapes to vendor [1/2].
    Signed-off-by: spezi77 <spezi7713@gmx.net>

--- packages/overlays/Android.mk
+++ packages/overlays/Android.mk
@@ -54,9 +54,6 @@ LOCAL_REQUIRED_MODULES := \
 	IconPackRoundedSettingsOverlay \
 	IconPackRoundedSystemUIOverlay \
 	IconPackRoundedThemePickerOverlay \
-	IconShapeRoundedRectOverlay \
-	IconShapeSquircleOverlay \
-	IconShapeTeardropOverlay \
 	NavigationBarMode3ButtonOverlay \
 	NavigationBarMode2ButtonOverlay \
 	NavigationBarModeGesturalOverlay \
