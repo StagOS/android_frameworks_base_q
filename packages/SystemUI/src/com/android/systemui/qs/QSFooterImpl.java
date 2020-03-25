Team-Xtreme/android_frameworks_base-1

SystemUI: Allow customizing footer text [1/2]
 * add toggle as well

Change-Id: I52cd9d51de01977095e0ba62950f9343a775e647

packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -45,7 +45,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
@@ -53,7 +52,6 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.settingslib.Utils;
-import com.android.settingslib.development.DevelopmentSettingsEnabler;
 import com.android.settingslib.drawable.UserIconDrawable;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
@@ -108,7 +106,7 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
     private OnClickListener mExpandClickListener;
     protected Vibrator mVibrator;
 
-    private final ContentObserver mDeveloperSettingsObserver = new ContentObserver(
+    private final ContentObserver mSettingsObserver = new ContentObserver(
             new Handler(mContext.getMainLooper())) {
         @Override
         public void onChange(boolean selfChange, Uri uri) {
@@ -176,12 +174,20 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
     private void setBuildText() {
         TextView v = findViewById(R.id.build);
         if (v == null) return;
-        if (DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(mContext)) {
-            v.setText(mContext.getString(
-                    com.android.internal.R.string.bugreport_status,
-                    Build.VERSION.RELEASE,
-                    Build.ID));
-            v.setVisibility(View.VISIBLE);
+        boolean isShow = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.FOOTER_TEXT_SHOW, 1,
+                        UserHandle.USER_CURRENT) == 1;
+        String text = Settings.System.getStringForUser(mContext.getContentResolver(),
+                        Settings.System.FOOTER_TEXT_STRING,
+                        UserHandle.USER_CURRENT);
+        if (isShow) {
+            if (text == null || text == "") {
+                v.setText("#LetsAIMify");
+                v.setVisibility(View.VISIBLE);
+            } else {
+                v.setText(text);
+                v.setVisibility(View.VISIBLE);
+            }
         } else {
             v.setVisibility(View.GONE);
         }
@@ -276,8 +282,11 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
         super.onAttachedToWindow();
         Dependency.get(TunerService.class).addTunable(this, QS_FOOTER_SHOW_SETTINGS);
         mContext.getContentResolver().registerContentObserver(
-                Settings.Global.getUriFor(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED), false,
-                mDeveloperSettingsObserver, UserHandle.USER_ALL);
+                Settings.System.getUriFor(Settings.System.FOOTER_TEXT_SHOW), false,
+                mSettingsObserver, UserHandle.USER_ALL);
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.FOOTER_TEXT_STRING), false,
+                mSettingsObserver, UserHandle.USER_ALL);
     }
 
     @Override
@@ -285,7 +294,7 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
     public void onDetachedFromWindow() {
         setListening(false);
         Dependency.get(TunerService.class).removeTunable(this);
-        mContext.getContentResolver().unregisterContentObserver(mDeveloperSettingsObserver);
+        mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
         super.onDetachedFromWindow();
     }
 

StagOS/android_frameworks_base_q

packages/SystemUI/src/com/android/systemui/qs/QSFooterImpl.java
@@ -44,7 +44,6 @@ import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
 
@@ -52,7 +51,6 @@ import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.keyguard.KeyguardUpdateMonitor;
 import com.android.settingslib.Utils;
-import com.android.settingslib.development.DevelopmentSettingsEnabler;
 import com.android.settingslib.drawable.UserIconDrawable;
 import com.android.systemui.Dependency;
 import com.android.systemui.R;
@@ -107,7 +105,7 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
 
     private OnClickListener mExpandClickListener;
 
-    private final ContentObserver mDeveloperSettingsObserver = new ContentObserver(
+    private final ContentObserver mSettingsObserver = new ContentObserver(
             new Handler(mContext.getMainLooper())) {
         @Override
         public void onChange(boolean selfChange, Uri uri) {
@@ -174,9 +172,20 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
     private void setBuildText() {
         TextView v = findViewById(R.id.build);
         if (v == null) return;
-        if (DevelopmentSettingsEnabler.isDevelopmentSettingsEnabled(mContext)) {
-            v.setText("#sicparvismagna");
-            v.setVisibility(View.VISIBLE);
+        boolean isShow = Settings.System.getIntForUser(mContext.getContentResolver(),
+                        Settings.System.FOOTER_TEXT_SHOW, 1,
+                        UserHandle.USER_CURRENT) == 1;
+        String text = Settings.System.getStringForUser(mContext.getContentResolver(),
+                        Settings.System.FOOTER_TEXT_STRING,
+                        UserHandle.USER_CURRENT);
+        if (isShow) {
+            if (text == null || text == "") {
+                v.setText("#sicparvismagna");
+                v.setVisibility(View.VISIBLE);
+            } else {
+                v.setText(text);
+                v.setVisibility(View.VISIBLE);
+            }
         } else {
             v.setVisibility(View.GONE);
         }
@@ -265,8 +274,11 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
         super.onAttachedToWindow();
         Dependency.get(TunerService.class).addTunable(this, QS_FOOTER_SHOW_SETTINGS);
         mContext.getContentResolver().registerContentObserver(
-                Settings.Global.getUriFor(Settings.Global.DEVELOPMENT_SETTINGS_ENABLED), false,
-                mDeveloperSettingsObserver, UserHandle.USER_ALL);
+                Settings.System.getUriFor(Settings.System.FOOTER_TEXT_SHOW), false,
+                mSettingsObserver, UserHandle.USER_ALL);
+        mContext.getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(Settings.System.FOOTER_TEXT_STRING), false,
+                mSettingsObserver, UserHandle.USER_ALL);
     }
 
     @Override
@@ -274,7 +286,7 @@ public class QSFooterImpl extends FrameLayout implements Tunable, QSFooter,
     public void onDetachedFromWindow() {
         setListening(false);
         Dependency.get(TunerService.class).removeTunable(this);
-        mContext.getContentResolver().unregisterContentObserver(mDeveloperSettingsObserver);
+        mContext.getContentResolver().unregisterContentObserver(mSettingsObserver);
         super.onDetachedFromWindow();
     }
 