ceeff5390b061b4b07cbaecc09765b626136ca81
vjspranav <pranavasri@live.in>
2020-04-09 22:43:14 +0530

    Revert "FODCircleView: fix incorrect position on keyguard"
    This reverts commit dc9b8f4724f677a78e536a9450f77a6da4d2d951.

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -33,6 +33,7 @@ import android.graphics.Paint;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
+import android.hardware.biometrics.BiometricSourceType;
 import android.hardware.fingerprint.FingerprintManager;
 import android.os.Handler;
 import android.os.UserHandle;
@@ -107,7 +108,7 @@ public class FODCircleView extends ImageView {
         R.drawable.fod_icon_cpt_america_flat,
         R.drawable.fod_icon_cpt_america_flat_gray,
         R.drawable.fod_icon_dragon_black_flat,
-        R.drawable.fod_icon_evo1,
+        R.drawable.fod_icon_future,
         R.drawable.fod_icon_glow_circle,
         R.drawable.fod_icon_neon_arc,
         R.drawable.fod_icon_neon_arc_gray,
@@ -117,10 +118,7 @@ public class FODCircleView extends ImageView {
         R.drawable.fod_icon_rainbow_horn,
         R.drawable.fod_icon_shooky,
         R.drawable.fod_icon_spiral_blue,
-        R.drawable.fod_icon_sun_metro,
-        R.drawable.fod_icon_scratch_red_blue,
-        R.drawable.fod_icon_scratch_pink_blue,
-        R.drawable.fod_icon_transparent
+        R.drawable.fod_icon_sun_metro
     };
 
     private int mPressedIcon;
@@ -169,7 +167,6 @@ public class FODCircleView extends ImageView {
         public void onKeyguardVisibilityChanged(boolean showing) {
             mIsKeyguard = showing;
             updateStyle();
-            updatePosition();
             if (mFODAnimation != null) {
                 mFODAnimation.setAnimationKeyguard(mIsKeyguard);
             }
@@ -534,11 +531,6 @@ public class FODCircleView extends ImageView {
                 break;
             default:
                 throw new IllegalArgumentException("Unknown rotation: " + rotation);
-        }
-
-        if (mIsKeyguard) {
-            mParams.x = mPositionX;
-            mParams.y = mPositionY;
         }
 
         if (mIsDreaming) {
