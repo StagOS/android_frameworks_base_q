19162806ce29a0bb00c337ab4c0b36e6daaa0cef
SZRADEV <ra.szymon87@gmai.com>
2020-07-13 18:21:47 +0200

    fix conficts

--- packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
+++ packages/SystemUI/src/com/android/systemui/biometrics/FODCircleView.java
@@ -33,8 +33,6 @@ import android.graphics.Paint;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
-import android.hardware.biometrics.BiometricSourceType;
-import android.hardware.fingerprint.FingerprintManager;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.os.Looper;
@@ -145,7 +143,6 @@ public class FODCircleView extends ImageView {
         }
     };
 
-    private FingerprintManager mFingerprintManager;
     private KeyguardUpdateMonitor mUpdateMonitor;
 
     private KeyguardUpdateMonitorCallback mMonitorCallback = new KeyguardUpdateMonitorCallback() {
@@ -229,7 +226,6 @@ public class FODCircleView extends ImageView {
     };
 
     private void dispatchFodScreenStateChanged(boolean interactive){
-        dispatchFodFingerprintHasEnrolledFinger();
         if (mFodScreenOffHandler != null){
             mFodScreenOffHandler.onScreenStateChanged(interactive);
         }
@@ -244,15 +240,6 @@ public class FODCircleView extends ImageView {
     private void dispatchFodDreamingStateChanged(){
         if (mFodScreenOffHandler != null){
             mFodScreenOffHandler.onDreamingStateChanged(mIsDreaming);
-        }
-    }
-
-    private void dispatchFodFingerprintHasEnrolledFinger(){
-        if (mFodScreenOffHandler != null &&
-                mFingerprintManager != null && 
-                mFingerprintManager.isHardwareDetected()) {
-            boolean enrolled = mFingerprintManager.hasEnrolledFingerprints(UserHandle.myUserId());
-            mFodScreenOffHandler.hasEnrolledFingerprints(enrolled);
         }
     }
 
@@ -279,7 +266,6 @@ public class FODCircleView extends ImageView {
     public FODCircleView(Context context, FodScreenOffHandler fodScreenOffHandler) {
         super(context);
 
-        mFingerprintManager = (FingerprintManager) context.getSystemService(Context.FINGERPRINT_SERVICE);
         mFodScreenOffHandler = fodScreenOffHandler;
 
         IFingerprintInscreen daemon = getFingerprintInScreenDaemon();
